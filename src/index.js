3ce02e0adf3f902958f63a4096362b53bb52f4e2
Ranisans <ranisans@yahoo.com>
2019-10-04 15:35:38 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const timeToMasterJS = 800;
+const timeToBasicProgramming = 500;
+
 /**
  * @param preferences - target student focus
  * @param knowsProgramming - if student can do programming and know basics
@@ -5,10 +8,13 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = "family",
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  const hoursToMaster = knowsProgramming
+    ? timeToMasterJS
+    : timeToBasicProgramming + timeToMasterJS;
+
+  return Math.ceil(hoursToMaster / config[focus]);
+};
